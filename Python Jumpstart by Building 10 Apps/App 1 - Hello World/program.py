5fb14731081dd7f3889632f22bab95a6accbde6f
pedrocc <pedrocc@me.com>
2016-06-05 22:00:47 -0300

    Criando o Guess the Number Game app.

--- Python Jumpstart by Building 10 Apps/App 1 - Hello World/program.py
+++ Python Jumpstart by Building 10 Apps/App 1 - Hello World/program.py
@@ -2,5 +2,6 @@ print('-----------------------------')
 print('          HELLO APP')
 print('-----------------------------', '\n')
 
+
 name = input('What is your name? ')
 print('Nice to meet you Michael', name)
