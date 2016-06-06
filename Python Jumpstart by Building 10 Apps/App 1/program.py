5358ad33380e9b5a8711cd4b043204a19bf1845f
pedrocc <pedrocc@me.com>
2016-06-05 21:40:30 -0300

    - Setando o ambiente inicial - Criando o primeiro programa Hello World

---
+++ Python Jumpstart by Building 10 Apps/App 1/program.py
@@ -0,0 +1,6 @@
+print('-----------------------------')
+print('          HELLO APP')
+print('-----------------------------', '\n')
+
+name = input('What is your name? ')
+print('Nice to meet you Michael', name)
